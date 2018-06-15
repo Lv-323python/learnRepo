1020f953ad7007b4b1e35c89ceafb0c513f00221
Anastasiia-Khab <anastasykhab@gmail.com>
2018-06-15 16:20:16 +0300

    Changed task 224

--- tasks/chapter7.py
+++ tasks/chapter7.py
@@ -15,28 +15,11 @@ def task_224(num):
     dividers = []
     if num == 0:
         return [0]
-    dividers.append(1)
-    for natural in range(2, int(num + 1)):
+    for natural in range(1, int(num + 1)):
         if num % natural == 0:
             dividers.append(natural)
-            dividers += task_224_find_other_dividers(int(num / natural))
-            break
     return dividers
 
-
-def task_224_find_other_dividers(num):
-    """
-    Дано натуральное число n. Получить все его натуральные делители.
-    :param num: natural
-    :return: tuple of all natural dividers of num
-    """
-    dividers = []
-    for natural in range(2, int(num + 1)):
-        if num % natural == 0:
-            dividers.append(natural)
-            dividers += task_224_find_other_dividers(int(num / natural))
-            break
-    return dividers
 
 
 def task_225(num):
